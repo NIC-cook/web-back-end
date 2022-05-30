d0e80b2692da53bf19b96e20cdd967e6ae29cc09
NIC-cook <78019826+NIC-cook@users.noreply.github.com>
2022-05-31 00:47:16 +0800

    Add files via upload

---
+++ FTBmall-back/src/main/java/com/ftbmall/FtBmallApplication.java
@@ -0,0 +1,13 @@
+package com.ftbmall;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FtBmallApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FtBmallApplication.class, args);
+    }
+
+}
---
+++ FTBmall-back/src/test/java/com/ftbmall/FtBmallApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ftbmall;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FtBmallApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
